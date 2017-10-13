c35640d650862eac46834c114026d78938348449
Karim Vaes <kvaes@github.com>
2017-10-13 10:55:09 +0200

    Added an additional API function for a direct device SAS token approach

---
+++ getSASTokenByDeviceID-Secured/index.js
@@ -0,0 +1,18 @@
+module.exports = function (context, req) {
+    var keyName = process.env.keyname;
+    var keyValue = process.env.keyvalue;
+    var deviceId = req.query.deviceid;
+    var iothubHost = process.env.iothubhostname+'/devices/'+deviceId;
+
+    // "myhub.azure-devices.net/devices/device1";
+    // var sas = serviceSas.create(connStr.HostName, connStr.SharedAccessKeyName, connStr.SharedAccessKey, anHourFromNow()).toString();
+    var serviceSdk = require('azure-iot');
+	var serviceSas = require('azure-iot').SharedAccessSignature;
+    var anHourFromNow = require('azure-iot-common').anHourFromNow;
+    var sasTokenDevice = serviceSas.create(iothubHost, keyName, keyValue, anHourFromNow()).toString();
+
+    context.res = {
+      sastoken: sasTokenDevice
+    };
+    context.done();
+};
