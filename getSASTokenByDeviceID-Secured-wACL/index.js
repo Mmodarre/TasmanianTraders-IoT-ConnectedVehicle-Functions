929dbb18b215fe3bb667b9784d655e6b8f56bf97
Karim Vaes <kvaes@github.com>
2017-10-18 20:39:01 +0200

    With IP Allow

--- getSASTokenByDeviceID-Secured-wACL/index.js
+++ getSASTokenByDeviceID-Secured-wACL/index.js
@@ -1,22 +1,29 @@
-module.exports = function (context, req) {
-    var keyName = process.env.keyname;
-    var keyValue = process.env.keyvalue;
-    var deviceId = req.query.deviceid;
-    var iothubHost = process.env.iothubhostname+"/devices/"+deviceId;
-    var get_ip = require('ipware')().get_ip;
-    var ip_info = get_ip(req);
-    console.log(ip_info);
+getClientAddress = function (req) {
+  return (req.headers['x-forwarded-for'] || '').split(':')[0] 
+  || req.connection.remoteAddress;
+};
 
-    // "myhub.azure-devices.net/devices/device1";
-    // var sas = serviceSas.create(connStr.HostName, connStr.SharedAccessKeyName, connStr.SharedAccessKey, anHourFromNow()).toString();
-    var serviceSdk = require('azure-iothub');
-	var serviceSas = require('azure-iothub').SharedAccessSignature;
-    var expiresInMins = 60;
-    var expires = (Date.now() / 1000) + expiresInMins * 60;
-    var sasTokenDevice = serviceSas.create(iothubHost, keyName, keyValue, expires);
-    var sasTokenDeviceString = sasTokenDevice.toString();
-    context.res = {
-      sastoken: sasTokenDeviceString
-    };
-    context.done();
+module.exports = function (context, req) {
+  var keyName = process.env.keyname;
+  var keyValue = process.env.keyvalue;
+  var deviceId = req.query.deviceid;
+  var iothubHost = process.env.iothubhostname+"/devices/"+deviceId;
+  var ip_info = getClientAddress(req);
+  var allowedIP = process.env.allowedip;
+  if (allowedIP != ip_info) {
+    context.log("IP Address not allowed!");
+  }
+
+  // "myhub.azure-devices.net/devices/device1";
+  // var sas = serviceSas.create(connStr.HostName, connStr.SharedAccessKeyName, connStr.SharedAccessKey, anHourFromNow()).toString();
+  var serviceSdk = require('azure-iothub');
+  var serviceSas = require('azure-iothub').SharedAccessSignature;
+  var expiresInMins = 60;
+  var expires = (Date.now() / 1000) + expiresInMins * 60;
+  var sasTokenDevice = serviceSas.create(iothubHost, keyName, keyValue, expires);
+  var sasTokenDeviceString = sasTokenDevice.toString();
+  context.res = {
+    sastoken: sasTokenDeviceString
+  };
+  context.done();
 };
